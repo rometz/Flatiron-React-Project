fd63cc68beec732cc325a37c47a99c74a25868ba
Rollin Metzger <alcesalces@github.com>
2019-05-25 11:59:03 -0500

    Update App.js to make it a component

--- mtg-react-app/app/client/src/App.js
+++ mtg-react-app/app/client/src/App.js
@@ -1,17 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import AppRouter from './router/AppRouter';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <AppRouter />
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <AppRouter />
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
