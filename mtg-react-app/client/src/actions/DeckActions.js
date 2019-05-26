9b8aadd791474bacc0771c19da9e45eacc6744eb
Rollin Metzger <alcesalces@github.com>
2019-05-26 18:17:08 -0500

    Create deck action createDeck

--- mtg-react-app/client/src/actions/DeckActions.js
+++ mtg-react-app/client/src/actions/DeckActions.js
@@ -11,9 +11,17 @@ export function fetchDeck() {
 }
 
 export function createDeck(name, size) {
-
+    return async dispatch => {
+        const response = await fetch('draftingData', {
+            method: 'POST',
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ name, size })
+        });
+        const payload = await response.json();
+        dispatch({ type: "CREATE_DECK", payload });
+    }
 }
 
 export function deleteDeck(deckId) {
-    
+
 }
