e67261f67a1f7576afad41b317c80a9f05b6b755
Rollin Metzger <alcesalces@github.com>
2019-05-26 18:54:09 -0500

    Add delete_all functionality with card actions

--- mtg-react-app/client/src/actions/CardActions.js
+++ mtg-react-app/client/src/actions/CardActions.js
@@ -1,3 +1,36 @@
-export function createCard(<<params>>) {
-    
-}
+export function createCard(/* card params */) {
+    return async dispatch => {
+        const response = await fetch('cardData', {
+            method: 'POST',
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ /* card params */ })
+        });
+        const payload = await response.json();
+        dispatch({ type: "CREATE_CARD", payload });
+    };
+};
+
+export function deleteCards() {
+    return async dispatch => {
+        const response = await fetch('/api/cards/', {
+            method: 'DELETE',
+            headers: { 'Content-Type': 'application/json' }
+        });
+        const payload = await response.json();
+        dispatch({ type: "DELETE_CARDS", payload });
+    };
+};
+
+/* fetch a certain card from the deck, added later
+export function fetchDeck() {
+    return async (dispatch) => {
+        dispatch({type: "LOADING_DECK"});
+        const targetUrl = '/api/decks/';
+        const response = await fetch(targetUrl);
+        const set = await response.json()[0];
+        return dispatch({
+            type: 'FETCH_DECK', payload: set
+        });
+    };
+};
+*/
--- mtg-react-app/config/routes.rb
+++ mtg-react-app/config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   scope '/api' do
     resources :decks
-    resources :cards
+    resources :cards do
+      delete :index, on: :collection, action: :delete_all
+    end
   end
+
 end
