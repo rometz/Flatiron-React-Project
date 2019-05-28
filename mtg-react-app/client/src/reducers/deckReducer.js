65f405c22569806cf56729ef4c8f99cd0e82e049
Rollin Metzger <alcesalces@github.com>
2019-05-28 17:34:23 -0500

    Create deckReducer.js

---
+++ mtg-react-app/client/src/reducers/deckReducer.js
@@ -0,0 +1,23 @@
+function deckReducer(state = {
+    loading: false,
+    deckData: []
+}, action) {
+    switch (action.type) {
+        case "LOADING_DECK":
+            return {...state, loading: true, deckData: []}
+
+        case "FETCH_DECK":
+            return {...state, loading: false, deckData: action.payload}
+
+        case "CREATE_DECK":
+            return {...state, deckData: [...state.deckData, action.payload]}
+
+        case "DELETE_DECK":
+            return {...state, loading: false, deckData: state.deckData.filter(
+                deck => deck.id !== action.payload.id
+            )}
+
+        default:
+            return state
+    }
+}
