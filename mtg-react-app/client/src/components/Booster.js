fe11042cf381174754c2ed705436efa42d9040fe
Rollin Metzger <alcesalces@github.com>
2019-06-02 14:43:26 -0500

    Pass state of drafterContainer to props

--- mtg-react-app/client/src/components/Booster.js
+++ mtg-react-app/client/src/components/Booster.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import CardMapper from './Cards';
-import { connect } from 'react-redux';
+//import { connect } from 'react-redux';
 
 class Booster extends Component {
     state = {
--- mtg-react-app/client/src/containers/DrafterContainer.js
+++ mtg-react-app/client/src/containers/DrafterContainer.js
@@ -14,26 +14,33 @@ class DrafterContainer extends Component {
         this.props.addDeck(
             this.state.deck
         );
-        this.props.grabBooster();
     }
+
+    getStore
 
     render() {
         return (
             <div>
                 <Deck />
-                <Booster />
+                <Booster boosters={this.props.grabBooster}/>
             </div>
         )
     }
 }
 
-const mapStateToProps = ({ decks }) => ({ decks });
+const mapStateToProps = state => {
+    return {
+        decks: state.decks,
+        drafts: state.drafts,
+        cards: state.cards
+    }
+}
 
 const mapDispatchToProps = dispatch => ({
     addDeck: array => dispatch({type: "CREATE_DECK", array}),
     deleteDeck: id => dispatch({type: "DELETE_DECK", id}),
     deleteCards: text => dispatch({type: "DELETE_CARDS", text}),
-    grabBooster: text => dispatch({type: "FETCH_CARDS", text})
+    grabBooster: object => dispatch({type: "FETCH_CARDS", object})
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(DrafterContainer);
