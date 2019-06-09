f48c0443bc8b5732b9d9a562b6cfcae1a778b82b
Rollin Metzger <alcesalces@github.com>
2019-06-09 17:11:04 -0500

    Incorporate enhancer for redux store

--- mtg-react-app/client/src/index.js
+++ mtg-react-app/client/src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './reducers/rootReducer';
 import thunk from 'redux-thunk';
 import './index.css';
 import App from './App';
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(rootReducer, composeEnhancers(
+    applyMiddleware(thunk)
+));
 
 ReactDOM.render(
     <Provider store={store}>
