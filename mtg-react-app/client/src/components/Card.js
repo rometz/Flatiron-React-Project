a208230c996af071f39e933683e8839d61e529c7
Rollin Metzger <alcesalces@github.com>
2019-08-06 15:21:03 -0500

    Push minor changes.

--- mtg-react-app/client/src/components/Card.js
+++ mtg-react-app/client/src/components/Card.js
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Card = props => <div>
-    <ul>
-        <div className="mtg-card">
-            <li id="mtg-card-name" onClick={(e) => props.handleAddCard(e)} cardid={props.cardId}>{props.name}</li>
-            <li id="mtg-card-manaCost">{props.manaCost}</li>
-            <li id="mtg-card-text">{props.text}</li>
-        </div>
-        
-    </ul>
-</div>
+class Card extends Component {
+    state = {
+        favCounter: 0
+    }
+
+    handleFavCounter() {
+        this.setState({
+            favCounter: this.state.favCounter + 1
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <ul>
+                    <div className="mtg-card">
+                        <li id="mtg-card-name" onClick={(e) => this.props.handleAddCard(e)} cardid={this.props.cardId}>{this.props.name}</li>
+                        <li id="mtg-card-manaCost">{this.props.manaCost}</li>
+                        <li id="mtg-card-text">{this.props.text}</li>
+                        <li onClick={() => this.handleFavCounter()}>{this.state.favCounter}</li>
+                    </div>
+                </ul>
+            </div>
+        )
+    }
+
+}
+
+
 
 export default Card;
         
